c7a4908f187e3e0481f2d6f2bfca5e02d3946fce
pbrendon <brandonpetit23@yahoo.com>
2023-04-12 22:11:24 -0400

    Initialize project using Create React App

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import './App.css';
+
+function App() {
+  const title = 'welcome to the new blog';
+  const likes = 50;
+
+
+
+      return (
+    <div className="App">
+     <div className="content">
+       <h1>{title}</h1>
+       <p>liked {likes } </p>
+     </div>
+
+    </div>
+  );
+}
+
+export default App;
