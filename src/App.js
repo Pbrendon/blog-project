9b30b52b02f4f3295e76f6e100ef87149e735a12
pbrendon <brandonpetit23@yahoo.com>
2023-04-15 14:04:08 -0400

    Importing multiple diffrent component and nesting them

--- src/App.js
+++ src/App.js
@@ -1,17 +1,17 @@
 import './App.css';
-
+import Navbar from './Navbar';
+import Home from'./Home';
 function App() {
-  const title = 'welcome to the new blog';
-  const likes = 50;
-
 
 
       return (
+
     <div className="App">
+        <Navbar />
      <div className="content">
-       <h1>{title}</h1>
-       <p>liked {likes } </p>
-     </div>
+      <Home />
+
+      </div>
 
     </div>
   );
---
+++ src/Home.js
@@ -0,0 +1,9 @@
+const Home = () => {
+return (
+ <div classNmae="home">
+     <h2>Homepage</h2>
+ </div>
+);
+}
+
+export default Home;
